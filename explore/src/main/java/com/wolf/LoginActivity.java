25ffada46de05759c8d73be45eed3296f23174e4
jianying9 <ljy18659199765@gmail.com>
2014-03-13 20:37:44 +0800

    explore初始

---
+++ explore/src/main/java/com/wolf/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.wolf;
+
+import com.wolf.android.activity.AbstractWebSocketActivity;
+
+public class LoginActivity extends AbstractWebSocketActivity {
+
+
+    @Override
+    protected void onMessage(String message) {
+
+    }
+
+    @Override
+    protected String getServer() {
+        return null;
+    }
+}
