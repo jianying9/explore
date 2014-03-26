6be2c78527ab2dd8eb7d613b4f052ac56d30acd3
jianying9 <ljy18659199765@gmail.com>
2014-03-26 17:21:50 +0800

    测试jquery mobile页面

--- explore/src/main/java/com/wolf/LoginActivity.java
+++ explore/src/main/java/com/wolf/LoginActivity.java
@@ -3,6 +3,7 @@ package com.wolf;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebView;
 
 public class LoginActivity extends Activity {
@@ -10,6 +11,7 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         WebView webView = new WebView(this);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
